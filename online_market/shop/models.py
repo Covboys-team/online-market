e719b2e3431d195529ad190f4a124668575d6e6f
exites <or.strangelove@gmail.com>
2023-07-24 13:19:45 +0300

    tutorial django4 add shop app

---
+++ online_market/shop/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200,
+                            unique=True)
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                       args=[self.slug])
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products',
+                                 on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, db_index=True, verbose_name='URL')
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="products/%Y/%m/%d/", blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+        ]
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       args=[self.id, self.slug])
+
+
