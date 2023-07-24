6e10834bdfe66f5ebb8abbfc7bf31316174bf7ea
exites <or.strangelove@gmail.com>
2023-07-24 14:06:22 +0300

    tutorial django4 add cart(not working total price)

--- online_market/online_market/settings.py
+++ online_market/online_market/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
@@ -129,4 +130,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-CART_SESSION = 'cart'
+CART_SESSION_ID = 'cart'
