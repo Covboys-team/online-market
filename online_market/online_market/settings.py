3890d839522427533d71578d9bb946506db204e7
exites <or.strangelove@gmail.com>
2023-07-24 13:10:58 +0300

    tutorial django4 add shop app

--- online_market/online_market/settings.py
+++ online_market/online_market/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'discounts',
-    'orders',
-    'product'
+    'shop.apps.ShopConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +124,6 @@ STATICFILES_DIRS = []
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
